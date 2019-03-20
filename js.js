948219ef513a6d69c6e3ecf573e1c80f6657aaf6
Lucas Forte <36035857+lucasforte@users.noreply.github.com>
2019-03-19 21:29:36 -0300

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 function menuMob(id, active) { //parameters for element id and active class
     event.preventDefault(); //prevent default click
     let menu = document.getElementById(id); //capture de element id
-    const activeClass = active; //set the active class
+    let activeClass = active; //set the active class
 
     if (menu.classList.contains(activeClass)) { //check if element has active class
         menu.classList.remove(activeClass); //if it has, remove it
